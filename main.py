11883eca4d1bd9a7fdfe0f02c16f489a72d7f448
Al Fahad <Abdullah.Fahad@brainstation-23.com>
2023-04-24 00:26:33 +0600

    code refactored with game design pattern

--- main.py
+++ main.py
@@ -3,38 +3,48 @@ import pygame
 
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 
-pygame.init()
-window = pygame.display.set_mode((640,480))
-pygame.display.set_caption("My game with logo")
-pygame.display.set_icon(pygame.image.load("icon.png"))
-clock = pygame.time.Clock()
+class Game():
+    def __init__(self):
+        pygame.init()
+        self.window = pygame.display.set_mode((640,480))
+        pygame.display.set_caption("Discover Python & Patterns - https://www.patternsgameprog.com")
+        pygame.display.set_icon(pygame.image.load("icon.png"))
+        self.clock = pygame.time.Clock()
+        self.x = 120
+        self.y = 120
+        self.running = True
 
-x = 120
-y = 120
-running = True
-while running:
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-            break
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                running = False
+    def processInput(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
                 break
-            elif event.key == pygame.K_RIGHT:
-                x += 8
-            elif event.key == pygame.K_LEFT:
-                x -= 8
-            elif event.key == pygame.K_DOWN:
-                y += 8
-            elif event.key == pygame.K_UP:
-                y -= 8
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self.running = False
+                    break
+                elif event.key == pygame.K_RIGHT:
+                    self.x += 8
+                elif event.key == pygame.K_LEFT:
+                    self.x -= 8
+                elif event.key == pygame.K_DOWN:
+                    self.y += 8
+                elif event.key == pygame.K_UP:
+                    self.y -= 8
+    def update(self):
+        pass
 
-    window.fill((0,0,0))
-    pygame.draw.rect(window,(0,0,255),(x,y,400,240))
-    pygame.display.update()
+    def render(self):
+        self.window.fill((0,0,0))
+        pygame.draw.rect(self.window,(0,0,255),(self.x,self.y,400,240))
+        pygame.display.update()
 
-    clock.tick(60)
+    def run(self):
+        while self.running:
+            self.processInput()
+            self.update()
+            self.render()
+            self.clock.tick(60)
 
-pygame.quit()
+game = Game()
+game.run()
